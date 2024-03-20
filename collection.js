4268c54c02f12e938885b80076f98a9bb73e5c94
6lackb0y042 <collinsaninze@gmail.com>
2024-03-20 01:45:06 +0100

    sole

--- collection.js
+++ collection.js
@@ -1,8 +1,22 @@
-const mongoose = require ("mongoose")
+const { model, Schema } = require('mongoose')
+
+const roomTypeSchema = new Schema({
+    name: String
+})
+
+const roomSchema = new Schema({
+    name: String,
+    roomType: {
+        ref: "Room Type",
+        type: Schema.Types.ObjectId
+    },
+    price: Number,
+})
+
 const Room = model('Room', roomSchema)
 const RoomType = model('Room Type', roomTypeSchema)
 
 module.exports = {
     Room,
-    RoomType
+    RoomType
 }
